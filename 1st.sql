cfd2101db00924ab5c524ed2744bf1f8bcb34f65
HamnaFaisal1234 <umehafsa66@gmail.com>
2025-01-29 20:33:24 +0500

    Add files via upload

--- 1st.sql
+++ 1st.sql
@@ -1,21 +1,24 @@
-CREATE TABLE Student (
-    StudentID INT PRIMARY KEY,
-    NAME VARCHAR(255),
-    Age INT,
-    ADDRESS VARCHAR(255)
-);
+CREATE TABLE costumer(
+name VARCHAR(20) ,
+country VARCHAR(20),
+bill INT);
+INSERT INTO costumer ( name , country,bill) values
+('Hamna' ,' Pakistan ',2000),
+('John' ,' America ',3000),
+('David' ,' Pakistan ',4000),
+('Barbra' ,' India ',5000),
+('Alex' ,' Pakistan ',6000);
+Select DISTINCT country FROM costumer ;
+-- Count agreegate
+SELECT COUNT(name) FROM costumer WHERE country = ' Pakistan ';
+-- Average
+SELECT AVG(bill) FROM costumer ;
+--sum
+SELECT SUM(bill) FROM costumer ;
 
-INSERT INTO Student (StudentID, NAME, Age, ADDRESS) VALUES
-    (1, 'Ram', 18, 'Delhi'),
-    (2, 'Raj', 20, 'Mumbai'),
-    (3, 'Ramesh', 18, 'Bangalore'),
-    (4, 'Sujit', 25, 'Delhi'),
-    (5, 'Ram', 22, 'Chennai');
 
-SELECT * FROM Student WHERE Age = 18 AND ADDRESS = 'Delhi';
-SELECT * FROM Student WHERE NAME = 'Ram' AND Age = 18;
-SELECT * FROM Student WHERE NAME = 'Ram' OR NAME = 'Sujit';
-SELECT * FROM Student WHERE NAME = 'Ram' OR Age = 20;
-SELECT * FROM Student WHERE (Age = 18 AND NAME = 'Ram') OR NAME = 'Ramesh'; 
+
+
+
 
 
